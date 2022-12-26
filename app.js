71b98b68bab7f0c1c67ed6b9677305bb813accd8
Kevin <106387042+2dkn@users.noreply.github.com>
2022-12-26 09:22:46 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const shareButtons = document.querySelectorAll('.tile-share-button');
+console.log(shareButtons);
+
+const shareButtons2 = document.querySelectorAll('.share-button ')
+console.log(shareButtons2);
+
+async function copyText(e) {
+  e.preventDefault();
+  const link = this.getAttribute('link')
+    console.log(link);
+    try {
+       await navigator.clipboard.writeText(link)
+       alert("Copied the link:" + link)
+    } catch(err){
+        console.error(err)
+    }
+}
+shareButtons.forEach(function(shareButton, shareButtons2) {
+    shareButton.addEventListener('click', copyText)
+})
